7330a9af04716e2f53d7e28614d26bf9b5daf895
ellisbywater <egaraudy@gmail.com>
2021-01-27 14:43:05 -0700

    basic contract setup

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -2,4 +2,37 @@ pragma solidity ^0.5.0;
 
 contract Decentragram {
   // Code goes here...
+  string public name = "Decentragram";
+  uint public imageCount = 0;
+  // Store Images
+  mapping(uint => Image) public images;
+
+  struct Image {
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }  
+
+  event ImageCreated (
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
+
+  // Create Images
+  function uploadImage(string memory _imageHash, string memory _description) public {
+    imageCount = imageCount++;
+    images[imageCount] = Image(imageCount, _imageHash, _description, 0, msg.sender);
+
+    //Trigger an event
+    emit ImageCreated(imageCount, _imageHash, _description, 0, msg.sender);
+  }
+
+  // Tip Images
+
+
 }
